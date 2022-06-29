001097929161c28d959ccff4118eaed58cc0d946
Mahmudur Rahman <raianmr@gmail.com>
2022-06-29 18:37:58 +0600

    Procfile!!

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from . import models as mo
-from .config import settings as se
-from .database import engine
 from .routers import auth, posts, users, votes
 
-# mo.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(posts.router)
